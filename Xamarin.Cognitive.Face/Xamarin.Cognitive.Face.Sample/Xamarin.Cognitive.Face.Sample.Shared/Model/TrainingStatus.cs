b60ec511923deaf2c9a17375ce935599049a3982
Nate Rickard <nate.rickard@microsoft.com>
2017-05-30 15:44:04 -0400

    made TrainingStatus dates nullable

--- Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Shared/Model/TrainingStatus.cs
+++ Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Shared/Model/TrainingStatus.cs
@@ -6,9 +6,9 @@ namespace Xamarin.Cognitive.Face.Sample.Shared
 	{
 		public string Status { get; set; }
 
-		public DateTime CreatedDateTime { get; set; }
+		public DateTime? CreatedDateTime { get; set; }
 
-		public DateTime LastActionDateTime { get; set; }
+		public DateTime? LastActionDateTime { get; set; }
 
 		public string Message { get; set; }
 	}
