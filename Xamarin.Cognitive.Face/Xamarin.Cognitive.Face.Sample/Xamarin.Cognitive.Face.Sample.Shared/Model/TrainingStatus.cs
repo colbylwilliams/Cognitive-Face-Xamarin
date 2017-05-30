8d0e83f011974e3a5552afa1cc228977aeb6e651
Nate Rickard <nate.rickard@microsoft.com>
2017-05-30 14:47:46 -0400

    added TrainingStatus class

---
+++ Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Shared/Model/TrainingStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xamarin.Cognitive.Face.Sample.Shared
+{
+	public class TrainingStatus
+	{
+		public string Status { get; set; }
+
+		public DateTime CreatedDateTime { get; set; }
+
+		public DateTime LastActionDateTime { get; set; }
+
+		public string Message { get; set; }
+	}
+}
