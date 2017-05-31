a892e54bf7a1113bc68b6c57e87e9a76c0353b56
Nate Rickard <nate.rickard@microsoft.com>
2017-05-31 02:08:06 -0400

    added TaskQueue class

---
+++ Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Shared/Utilities/TaskQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.Cognitive.Face.Sample.Shared.Utilities
+{
+	public class TaskQueue
+	{
+		readonly SemaphoreSlim semaphore;
+
+
+		public TaskQueue (int concurrency = 1)
+		{
+			semaphore = new SemaphoreSlim (concurrency);
+		}
+
+
+		public async Task<T> Enqueue<T> (Func<Task<T>> taskGenerator)
+		{
+			await semaphore.WaitAsync ();
+
+			try
+			{
+				return await taskGenerator ();
+			}
+			finally
+			{
+				semaphore.Release ();
+			}
+		}
+
+
+		public async Task Enqueue (Func<Task> taskGenerator)
+		{
+			await semaphore.WaitAsync ();
+
+			try
+			{
+				await taskGenerator ();
+			}
+			finally
+			{
+				semaphore.Release ();
+			}
+		}
+	}
+}
