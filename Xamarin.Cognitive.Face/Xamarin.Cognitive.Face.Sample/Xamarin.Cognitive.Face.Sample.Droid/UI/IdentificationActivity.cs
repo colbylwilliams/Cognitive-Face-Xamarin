bb87ceee481ac63e10003c5c6362a77aaca03fbf
Nate Rickard <nate.rickard@microsoft.com>
2017-05-30 23:51:07 -0400

    [droid] hooked up manage person groups button on Identify screen

--- Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Droid/UI/IdentificationActivity.cs
+++ Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.Sample/Xamarin.Cognitive.Face.Sample.Droid/UI/IdentificationActivity.cs
@@ -1,7 +1,11 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
 
 namespace Xamarin.Cognitive.Face.Sample.Droid
 {
@@ -11,12 +15,39 @@ namespace Xamarin.Cognitive.Face.Sample.Droid
 			  ScreenOrientation = ScreenOrientation.Portrait)]
 	public class IdentificationActivity : AppCompatActivity
 	{
+		Button manageButton;
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
 			// Create your application here
 			SetContentView (Resource.Layout.activity_identification);
+
+			manageButton = FindViewById<Button> (Resource.Id.manage_person_groups);
+		}
+
+
+		protected override void OnResume ()
+		{
+			base.OnResume ();
+
+			manageButton.Click += managePersonGroups;
+		}
+
+
+		protected override void OnPause ()
+		{
+			manageButton.Click -= managePersonGroups;
+
+			base.OnPause ();
+		}
+
+
+		void managePersonGroups (object sender, EventArgs e)
+		{
+			Intent intent = new Intent (this, typeof (PersonGroupListActivity));
+			StartActivity (intent);
 		}
 	}
 }
