9f16c1de928bf89b4efd47a803310dee06979356
Nate Rickard <nate.rickard@microsoft.com>
2017-06-26 13:21:28 -0400

    [ios] added FixOrientation extension to MediaExtensions

--- Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.iOS/Extensions/MediaExtensions.cs
+++ Xamarin.Cognitive.Face/Xamarin.Cognitive.Face.iOS/Extensions/MediaExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using CoreGraphics;
 using UIKit;
 
@@ -56,6 +57,79 @@ namespace Xamarin.Cognitive.Face.Extensions
 			using (var data = image.AsJPEG ())
 			{
 				data.Save (path, true);
+			}
+		}
+
+
+		/// <summary>
+		/// Ensures that the UIImage is oriented in the UIImageOrientation.Up position.
+		/// </summary>
+		/// <returns>The image, oriented in the UIImageOrientation.Up position.</returns>
+		/// <param name="image">The Image to validate orientation for.</param>
+		public static UIImage FixOrientation (this UIImage image)
+		{
+			if (image.Orientation == UIImageOrientation.Up)
+			{
+				return image;
+			}
+
+			var transform = CGAffineTransform.MakeIdentity ();
+
+			switch (image.Orientation)
+			{
+				case UIImageOrientation.Down:
+				case UIImageOrientation.DownMirrored:
+					transform = CGAffineTransform.Translate (transform, image.Size.Width, image.Size.Height);
+					transform = CGAffineTransform.Rotate (transform, (float) Math.PI);
+					break;
+				case UIImageOrientation.Left:
+				case UIImageOrientation.LeftMirrored:
+					transform = CGAffineTransform.Translate (transform, image.Size.Width, 0);
+					transform = CGAffineTransform.Rotate (transform, (float) Math.PI / 2);
+					break;
+				case UIImageOrientation.Right:
+				case UIImageOrientation.RightMirrored:
+					transform = CGAffineTransform.Translate (transform, 0, image.Size.Height);
+					transform = CGAffineTransform.Rotate (transform, -(float) Math.PI / 2);
+					break;
+			}
+
+			switch (image.Orientation)
+			{
+				case UIImageOrientation.UpMirrored:
+				case UIImageOrientation.DownMirrored:
+					transform = CGAffineTransform.Translate (transform, image.Size.Width, 0);
+					transform = CGAffineTransform.Scale (transform, -1, 1);
+					break;
+				case UIImageOrientation.LeftMirrored:
+				case UIImageOrientation.RightMirrored:
+					transform = CGAffineTransform.Translate (transform, image.Size.Height, 0);
+					transform = CGAffineTransform.Scale (transform, -1, 1);
+					break;
+			}
+
+			using (var cgImg = image.CGImage)
+			using (var ctx = new CGBitmapContext (null, (nint) image.Size.Width, (nint) image.Size.Height, cgImg.BitsPerComponent, 0, cgImg.ColorSpace, cgImg.BitmapInfo))
+			{
+				ctx.ConcatCTM (transform);
+
+				switch (image.Orientation)
+				{
+					case UIImageOrientation.Left:
+					case UIImageOrientation.LeftMirrored:
+					case UIImageOrientation.Right:
+					case UIImageOrientation.RightMirrored:
+						ctx.DrawImage (new CGRect (0, 0, image.Size.Height, image.Size.Width), cgImg);
+						break;
+					default:
+						ctx.DrawImage (new CGRect (0, 0, image.Size.Width, image.Size.Height), cgImg);
+						break;
+				}
+
+				using (var newCgImg = ctx.ToImage ())
+				{
+					return UIImage.FromImage (newCgImg);
+				}
 			}
 		}
 	}
